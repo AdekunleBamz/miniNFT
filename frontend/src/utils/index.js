c000e807bd70b2761d86d8b21485ad8b3f57726d
AdekunleBamz <adekunlebamz@gmail.com>
2026-01-22 08:09:44 +0100

    chore(frontend): export animations and web3 utilities from index

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -6,9 +6,13 @@ export * from './format';
 export * from './validation';
 export * from './storage';
 export * from './nft';
+export * from './animations';
+export * from './web3';
 
 // Re-export defaults
 export { default as formatUtils } from './format';
 export { default as validationUtils } from './validation';
 export { default as storageUtils } from './storage';
 export { default as nftUtils } from './nft';
+export { default as animationUtils } from './animations';
+export { default as web3Utils } from './web3';

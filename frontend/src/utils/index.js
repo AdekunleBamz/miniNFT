609a8c4e52beb6a3ccab2a68aaebf9426e4c84ac
AdekunleBamz <bams.kunle@gmail.com>
2026-01-22 08:59:32 +0100

    chore(utils): export new date, string, and validation utilities

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -8,6 +8,9 @@ export * from './storage';
 export * from './nft';
 export * from './animations';
 export * from './web3';
+export * from './dateUtils';
+export * from './stringUtils';
+export * from './validationUtils';
 
 // Re-export defaults
 export { default as formatUtils } from './format';
@@ -16,3 +19,6 @@ export { default as storageUtils } from './storage';
 export { default as nftUtils } from './nft';
 export { default as animationUtils } from './animations';
 export { default as web3Utils } from './web3';
+export { default as dateUtils } from './dateUtils';
+export { default as stringUtils } from './stringUtils';
+export { default as validationUtilsNew } from './validationUtils';

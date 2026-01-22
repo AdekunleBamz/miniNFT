5ad36403263f826d69afa03495c8ea319c95dd36
AdekunleBamz <adekunlebamz@gmail.com>
2026-01-22 07:13:43 +0100

    chore(frontend): export useFavorites from hooks index

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -2,3 +2,4 @@ export { useNFTContract } from './useNFTContract';
 export { useTheme } from './useTheme';
 export { useConfetti } from './useConfetti';
 export { useSound } from './useSound';
+export { useFavorites } from './useFavorites';
